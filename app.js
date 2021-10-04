3e0e1b428955a9625a9c81da4709869ff038c2e5
Namhyuk Woo <alexnh97@gmail.com>
2021-10-03 17:46:07 -0700

    express session and flash set up

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const exp = require("constants");
 const express = require("express");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { campgroundSchema, reviewSchema } = require("./schemas");
-const Joi = require("joi");
+const session = require("express-session");
+const flash = require("connect-flash");
+// const { campgroundSchema, reviewSchema } = require("./schemas");
+// const Joi = require("joi");
 const ExpressError = require("./utils/ExpressError");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
-const Review = require("./models/review");
+// const Review = require("./models/review");
 
 const campgrounds = require("./routes/campground");
 const reviews = require("./routes/reviews");
 
-const Campground = require("./models/campground");
-const review = require("./models/review");
+// const Campground = require("./models/campground");
+// const review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -35,6 +37,25 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
