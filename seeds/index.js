46eb8d0689215d0f0b8969cacd58be55596dd487
Namhyuk Woo <alexnh97@gmail.com>
2021-10-18 18:53:01 -0700

    basic authorization done

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "616deba268d21239fb0cd7d8",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
